c490654fb9c0c3a30815ebbeb91927e89cc83176
Prasoon Jaiswal <114203333+prasoon-ms@users.noreply.github.com>
2026-04-10 20:36:45 +0000

    third commit

--- src/chat_app.py
+++ src/chat_app.py
@@ -14,6 +14,7 @@ import logging
 from opentelemetry.instrumentation.openai_v2 import OpenAIInstrumentor
 
 # Azure & OpenAI Imports
+# trigger GH workflow
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
 from openai import AzureOpenAI
